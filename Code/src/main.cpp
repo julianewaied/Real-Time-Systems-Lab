df00a5d1922eb09fcbb796e5ca98a1a4a1658e16
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-07-25 11:27:07 +0300

    all set up

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
 #include "../include/Utility.h"
 const char* path = "C:/Users/WIN10PRO/Desktop/My Stuff/University/BSC/Y3/RT systems/Real-Time-Systems-Lab/Code/src/test3.csv";
 int Run()
@@ -12,6 +16,10 @@ int Run()
     //std::cout << std::get<1>(v[0][0][0])<<std::endl;
     //file.setPath("None");
     //file.closeFile();
-    std::cout << "Hello World";
+    cv::Mat img = cv::imread("C:/Users/WIN10PRO/Pictures/fish.png");
+    namedWindow("Test", WINDOW_AUTOSIZE);
+    imshow("Test", img);
+    cv::waitKey(0);
+    cv::destroyAllWindows();
     return 0;
 }
