ffa254d0dbf4c3efb1002af1cb48ca01b36e781b
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-08-25 01:15:24 +0300

    project done!

--- Code/include/PointDisplayer.h
+++ Code/include/PointDisplayer.h
@@ -14,17 +14,21 @@ class PointDisplayer {
 	const int WIDTH = 1000;
 	string window_name;
 	
+	// fits points to the window
 	void fitPoints(vector<Point2i>& points) const;
+	// puts a circle on the required point in the image
 	void displayPoint(const Point2i& point, cv::Mat img) const;
 
 public:
 	PointDisplayer(string& window_name);
 
+	// wrapper for display(const vector<cv::Point2i>& points) const
 	void display(const vector<Eigen::Vector2d>& points) const;
+	// displays the points and plots them to the screen
 	void display(const vector<cv::Point2i>& points) const;
 
+	// displays the topdown view of the processed points
 	void topDownView(const vector<Eigen::Vector3d>& points) const;
-
 
 	// builds top-down view of all the files and plots it
 	static int BuildTDView(vector<string> mvFiles, vector<string> heightFiles);
